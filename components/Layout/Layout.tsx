dd5e59fcacf354d96821854fdb421750f1837919
Ricardo8Abreu <98675302+Ricardo8Abreu@users.noreply.github.com>
2023-03-03 01:31:50 -0500

    cambiando componente el Layout a arrow function

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -4,22 +4,21 @@ import Header from './Header/Header';
 import Modales from './Modales/Modales';
 import ModalContextProvider from '../../context/ModalContext/ModalContext';
 
-export default function Layout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+const Layout = ({ children }) => {
   return (
-      <div className=' flex flex-col h-screen w-screen text-black'>
-        <ModalContextProvider>
+    <div className=' flex flex-col h-screen w-screen text-black'>
+      <ModalContextProvider>
 
-          <Modales />
-          <Header />
-          <main className='bg-white grow h-[300px]'>
-            {children}
-          </main>
-          <Footer />
-        </ModalContextProvider>
-      </div>
-  );
+        <Modales />
+        <Header />
+        <main className='bg-white grow h-[300px]'>
+          {children}
+        </main>
+        <Footer />
+      </ModalContextProvider>
+    </div>
+  )
 }
+
+export default Layout
+
