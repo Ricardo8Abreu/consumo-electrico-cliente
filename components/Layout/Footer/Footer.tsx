eeafa63be63b14dff0bf2cfd075f8d9dab80a77a
Ricardo8Abreu <98675302+Ricardo8Abreu@users.noreply.github.com>
2023-03-03 03:11:35 -0500

    corrigiendo falla ortográfica

--- components/Layout/Footer/Footer.tsx
+++ components/Layout/Footer/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
     return (
         <footer className=" p-3 font-bold text-center bg-[#FFEA20]">
             <h2>
-                PandaTeamsDevs .Inc 2023
+                PandasTeamDevs .Inc 2023
             </h2>
 
         </footer>
