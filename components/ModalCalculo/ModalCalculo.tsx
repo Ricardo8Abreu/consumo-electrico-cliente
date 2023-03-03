609bc119497e1e709d2537a1b09ff11bd711c058
Ricardo8Abreu <98675302+Ricardo8Abreu@users.noreply.github.com>
2023-03-03 02:16:52 -0500

    css, mejorando el css del modal calculo

--- components/ModalCalculo/ModalCalculo.tsx
+++ components/ModalCalculo/ModalCalculo.tsx
@@ -8,19 +8,23 @@ const ModalCalculo = ({/* data, */ cerrar }) => {
     const { kw, importe } = data
 
     return (
-        <div className="flex items-center justify-center rounded-[30px] py-5 px-8 flex-col w-[300px]  text-white bg-[#8dcbe6]"
-            onClick={(e)=> e.stopPropagation()}
+        <div className="flex gap-2 items-center justify-center rounded-[30px] py-5 px-8 flex-col w-[320px]  text-white bg-[#8dcbe6]"
+            onClick={(e) => e.stopPropagation()}
         >
-            <div className="flex w-full font-bold justify-between">
+            <div className="flex items-center w-full text-2xl font-bold justify-between">
                 <h2>Importe</h2>
 
-                <span className=" " color="inherit" onClick={()=>cerrar()}>X</span>
+                <button className=" cursor-pointer p-1 font-extrabold " color="inherit"
+                    onClick={() => cerrar()}
+                >X</button>
             </div>
 
-            <div className="flex gap-5 items-center py-5 ">
-                <BsFillLightningFill className=" text-[#FFEA20]  h-20 text-9xl " />
+            <div className="flex gap-1 items-center p-2  ">
+                <div className="flex text-[60px]">
+                    <BsFillLightningFill className=" text-[#FFEA20] " />
+                </div>
 
-                <p>Usted ha consumido {kw} KW por tanto su importe es de {importe} CUP</p>
+                <p className="font-bold">Usted ha consumido {kw} KW por tanto su importe es de {importe} CUP</p>
             </div>
         </div>
     )
