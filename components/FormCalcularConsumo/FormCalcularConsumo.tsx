ea80cb49a491a84962fe44f85d5be041f9627a42
Ricardo8Abreu <98675302+Ricardo8Abreu@users.noreply.github.com>
2023-03-03 01:54:28 -0500

    css, corrigiendo  flex direction del form consumo

--- components/FormCalcularConsumo/FormCalcularConsumo.tsx
+++ components/FormCalcularConsumo/FormCalcularConsumo.tsx
@@ -8,15 +8,16 @@ const FormCalcularConsumo = ({  }) => {
     const { handleCalcularConsumo, dataFormInput } = useFormCalcular()
 
     return (
-        <form className="flex p-6 font-bold flex-col sm:flex-row  gap-7 items-center justify-center"
+        <form className="flex p-6 font-bold flex-col border w-full gap-7 items-center justify-center"
             onSubmit={handleCalcularConsumo}
             
         >
-            <div className="flex gap-7 w-[400px] justify-center items-center grow">
+            <div className="flex flex-col sm:flex-row gap-7 w-[400px] justify-center items-center grow">
                 <FormInputs
                     data={dataFormInput}
                 />
             </div>
+
             <button type='submit' className=' h-max rounded-xl py-2 text-white px-2 bg-[#FFEA20] border border-[#a79815]'>
                 Ver consumo
             </button>
